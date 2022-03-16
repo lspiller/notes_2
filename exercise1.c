2dfc39812d6bf0ec826a995a84d69f6342adbe65
lspiller <lspiller@users.noreply.github.com>
2022-03-16 13:44:53 +1100

    Create exercise1.c

---
+++ exercise1.c
@@ -0,0 +1,12 @@
+long stack_check( long a,  long b,  long c,  long d,  
+                  long e,  long f,  long g,  long h ) {
+    long xx = a * b * c * d * e * f * g * h;
+	long yy = a + b + c + d + e + f + g + h;
+    long zz = xx % yy;
+    return zz + 20;
+}
+
+int main() {
+    stack_check(11,  22,  33, 44, 66, 77, 88);
+	return 0;
+}
