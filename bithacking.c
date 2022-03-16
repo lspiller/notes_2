3f767eb9639fe202a0f86920ae995a3eaf573dc6
lspiller <lspiller@users.noreply.github.com>
2022-03-16 14:38:00 +1100

    Update bithacking.c

--- bithacking.c
+++ bithacking.c
@@ -2,7 +2,7 @@
 
 // F1
 int x, y;
-bool r = ((x ^ y) < 0);
+int r = ((x ^ y) < 0);
 
 // F2
 int v;
@@ -14,3 +14,16 @@ int x;
 int y;
 
 int r = y ^ ((x ^ y) & -(x < y));
+
+// F4
+unsigned int v;
+int r = (v & (v - 1)) == 0;
+
+// F5
+unsigned int v; // v < 32 bits
+unsigned int r;
+
+r =  ((v & 0xfff) * 0x1001001001001ULL & 0x84210842108421ULL) % 0x1f;
+r += (((v & 0xfff000) >> 12) * 0x1001001001001ULL & 0x84210842108421ULL) % 
+     0x1f;
+r += ((v >> 24) * 0x1001001001001ULL & 0x84210842108421ULL) % 0x1f;
