541b1e98dde241c7b0b9a513b5445fbf43987129
lspiller <lspiller@users.noreply.github.com>
2022-03-16 14:26:25 +1100

    Update obs8.c

--- obs8.c
+++ obs8.c
@@ -2,21 +2,21 @@ void send(char* to,  char* from,  int count) {
 		int register n = (count + 7) / 8;
 		switch (count % 8) {
 	    	case 0:
-				do { *to = *from++;
+			do { *to = *from++;
 	    	case 7:
-		    	 *to = *from++;
+		    	 *to++ = *from++;
 	    	case 6:
-		     	*to = *from++;
-			case 5:
-				*to = *from++;
+		     	*to++ = *from++;
+		case 5:
+			*to++ = *from++;
   	  	case 4:
-				*to = *from++;
+			*to++ = *from++;
 	    	case 3:
-		     	*to = *from++;
+		     	*to++ = *from++;
 	    	case 2:
-		     	*to = *from++;
+		     	*to++ = *from++;
 	    	case 1: 
-		    	*to = *from++;
+		    	*to++ = *from++;
 	            } while (--n > 0);
  	}
 } 
